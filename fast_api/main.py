09313cc3d06f16a2223e8f67b00925b9e51c1f75
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-08-21 06:49:23 +0300

    обновил

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,43 +1,60 @@
 import logging
 
-from alembic.config import Config
-from alembic.command import upgrade
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from alembic.config import Config
+# from alembic.command import upgrade
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# import uvicorn
+#
+# from api import layer, layer_type, dashboard, apitesting
+#
+# from db.db import engine, init_db
+# from core.config import CORS_ORIGINS
+# from core.logger import LOGGING
+# from seeds.seeder import seed_db
+
+
+# app = FastAPI(
+#     title='Interactive Map',
+#     description="Backend for Interactive Map",
+#     docs_url='/api/openapi',
+# )
+
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=CORS_ORIGINS,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+# app.include_router(layer.router, prefix='/api/v1/layer')
+# app.include_router(layer_type.router, prefix='/api/v1/layer_type')
+# app.include_router(dashboard.router, prefix='/api/v1/dashboard')
+
+
+
+
+
+# @app.on_event('startup')
+# async def startup():
+#     init_db()
+#     # upgrade(Config("./alembic.ini"), "head")
+#     seed_db()
+
+
+
 import uvicorn
-
-from api import layer, layer_type, dashboard
-from db.db import engine, init_db
-from core.config import CORS_ORIGINS
 from core.logger import LOGGING
-from seeds.seeder import seed_db
+from fastapi import FastAPI
+from api import apitesting
 
 
-app = FastAPI(
-    title='Interactive Map',
-    description="Backend for Interactive Map",
-    docs_url='/api/openapi',
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(layer.router, prefix='/api/v1/layer')
-app.include_router(layer_type.router, prefix='/api/v1/layer_type')
-app.include_router(dashboard.router, prefix='/api/v1/dashboard')
-
-
-@app.on_event('startup')
-async def startup():
-    init_db()
-    # upgrade(Config("./alembic.ini"), "head")
-    seed_db()
-
+app = FastAPI()
+app.include_router(apitesting.router, prefix='/api/v1/apitesting')
 
 def main() -> None:
     uvicorn.run(
